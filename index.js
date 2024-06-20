bb9bd1af6c9dd2776f198cd3bd326d3bae24d791
5anne <tahaminaanne@gmail.com>
2024-06-20 20:21:35 +0600

    seventh commit

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://chakri-bazar-8e75f.web.app",
+        "https://chakri-bazar-8e75f.firebaseapp.com",
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 
 
@@ -23,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const jobJsonCollection = client.db('chakriBazarDB').collection('jobsJson');
         const blogsCollection = client.db('chakriBazarDB').collection('blogsData');
@@ -146,7 +153,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
